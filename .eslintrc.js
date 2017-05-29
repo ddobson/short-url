9e827da98dc73db7f6fcab59bb45b9ae98f907de
ddobson <ddobson445@gmail.com>
2017-05-29 10:44:25 -0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        },
+        "sourceType": "module"
+    },
+    "rules": {
+        "prefer-const": 2,
+        "prefer-arrow-callback": 1,
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-var": 2,
+        "dot-notation": [
+            "warn"
+        ],
+        "object-curly-spacing": [
+            "warn",
+            "always"
+        ],
+    }
+};
